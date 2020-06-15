309b8385c0c7bb0f395c5d08a7f088e1e4aa613b
corina <corina.schambacher@gmail.com>
2020-06-15 13:19:05 -0700

    minor style change

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './preview-collections.styles.scss';
+
+const CollectionPreview = () => (
+    <div className='collection-preview'>
+        <h1 className='title'> My Title</h1>
+        <div className='preview'>
+
+        </div>
+    </div>
+)
