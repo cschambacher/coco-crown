fbd9324ed2b6411f9ee23af5f1c4c43eb61d3a44
corina <corina.schambacher@gmail.com>
2020-12-01 14:12:20 -0800

    collection-item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import './collection-item.styles.scss'
 
-const collectionItem = () => (
+const collectionItem = ({id, name, price, imageUrl}) => (
     <div className='collection-item'>
-        <div>
+        <div
+            className = 'image'
+            style = {{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
 
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
         </div>
     </div>
 )
