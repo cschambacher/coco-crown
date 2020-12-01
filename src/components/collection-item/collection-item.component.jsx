15ac14737d7fb9d2f737ff2e107ec5f129d3a70a
corina <corina.schambacher@gmail.com>
2020-12-01 14:08:37 -0800

    collection item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './collection-item.styles.scss'
+
+const collectionItem = () => (
+    <div className='collection-item'>
+        <div>
+
+        </div>
+    </div>
+)
