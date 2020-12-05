6eb0c727be51c5b6db72e882e0a328e41919c000
corina <corina.schambacher@gmail.com>
2020-12-04 18:59:50 -0800

    added signin and signup component and created signin route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
+
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
@@ -18,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
