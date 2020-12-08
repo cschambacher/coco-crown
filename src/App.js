55ae56e08265babb7b982b47299ad653dffa9a8a
corina <corina.schambacher@gmail.com>
2020-12-07 20:50:01 -0800

    added unsuscribeFromAuth method on App.js

--- src/App.js
+++ src/App.js
@@ -18,11 +18,17 @@ class App extends React.Component{
       currentUser: null
     }
   }
+  unsuscribeFromAuth = null
+
   componentDidMount(){
-    auth.onAuthStateChanged(user => {
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({currentUser: user})
       console.log(user)
     })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
   }
   render (){
     return (
