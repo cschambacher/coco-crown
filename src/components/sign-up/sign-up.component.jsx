507329d27e78bd92b45597c0624fd6ec949ceb2b
corina <corina.schambacher@gmail.com>
2020-12-09 09:46:32 -0800

    sign up form

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -16,8 +16,54 @@ class SignUp extends React.Component {
             displayName: '',
             email: '',
             password: '',
+            confirmPassword: ''
         }
     }
+    render(){
+        const {displayName, email, pasword, confirmPassword} = this.state;
+        return (
+            <div className='sign-up'>
+                <h2 className='title'>
+                    I do not have an account
+                </h2>
+                <span> Sign up with your email and password</span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type='text'
+                        name='displayName'
+                        value={displayName}
+                        onChange={this.handleChange}
+                        label='Display Name'
+                        required
+                    />
+                    <FormInput
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={this.handleChange}
+                        label='Email'
+                        required
+                    />
+                    <FormInput
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={this.handleChange}
+                        label='Password'
+                        required
+                    />
+                    <FormInput
+                        type='password'
+                        name='confirmPassword'
+                        value={confirmPassword}
+                        onChange={this.handleChange}
+                        label='Confirm Password'
+                        required
+                    />
+                    <CustomButton type='submit'> SIGN UP </CustomButton>
+                </form>
+            </div>
+        )
+    }
 
-    
 }
