7184d63cac397075db1bd9bc1bf57134360d5525
corina <corina.schambacher@gmail.com>
2020-12-30 11:13:06 -0800

    added itemCount to cart-dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -18,5 +18,11 @@ const mdtp = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mstp = ({cart: {cartItems}})=>({
+    //itemCOunt is a selector: we pull all state and select some info
+    itemCount: cartItems.reduce(
+        (accQty, cartItem)=> accQty + cartItem.quantity, 0)
+})
 
-export default connect (null, mdtp)(CartIcon);
+
+export default connect (mstp, mdtp)(CartIcon);
