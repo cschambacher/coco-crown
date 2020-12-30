dc28d43cd457d0c7b458f409cdc36a3476e702fa
corina <corina.schambacher@gmail.com>
2020-12-30 15:57:50 -0800

    created checkout page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component'
 
 import './App.css';
@@ -58,6 +60,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
           <Route exact path='/signin' render={()=> 
             this.props.currentUser ? (
             <Redirect to='/'/>
