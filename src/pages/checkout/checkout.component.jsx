03f29e6fbb2e1c147c0a6af3ae67a0f2444d07fc
corina <corina.schambacher@gmail.com>
2021-01-03 14:46:49 -0800

    checkout page and total price cart selector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+// import { withRouter } from 'react-router-dom';
+
+// import CustomButton from '../custom-button/custom-button.component';
+// import CartItem from '../../cart-item/cart-item.component';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
             <div className='header-block'>
@@ -21,7 +28,18 @@ const CheckoutPage = () => (
                 <span>Remove</span>
             </div>
         </div>
+        {
+            cartItems.map(cartItem => 
+                cartItem.name
+            )
+        }
+        <div className='total'>
+            <span>TOTAL: ${total}</span>
+        </div>
     </div>
 )
-
-export default CheckoutPage
+const mstp = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
+export default connect(mstp)(CheckoutPage)
