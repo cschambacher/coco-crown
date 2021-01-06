9487fe19cec7cb6b2b89305fce89ff33d09428b4
corina <corina.schambacher@gmail.com>
2021-01-05 16:05:44 -0800

    added remove button and action to checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { createStructuredSelector } from 'reselect';
 // import { withRouter } from 'react-router-dom';
 
 // import CustomButton from '../custom-button/custom-button.component';
 // import CartItem from '../cart-item/cart-item.component';
-// import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { clearItemFromCart } from '../../redux/cart/cart.actions'
 // import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: {name, imageUrl, price, quantity} }) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt="item"/>
+const CheckoutItem = ({ cartItem, clearItem}) => {
+    const {name, imageUrl, price, quantity} = cartItem;
+    return(
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt="item"/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>{quantity}</span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={()=> clearItem(cartItem)}>&#10005;</div>
+
         </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
+    )
+};
 
-    </div>
-);
-
-export default CheckoutItem;
+const mdtp = dispatch => ({
+    clearItem: item=> dispatch(clearItemFromCart(item))
+})
+export default connect(null, mdtp)(CheckoutItem);
