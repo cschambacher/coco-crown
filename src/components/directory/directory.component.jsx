6023b5f67bb40542eec3167064d30b327adbfed3
corina <corina.schambacher@gmail.com>
2021-01-22 08:41:06 -0800

    halfway through shop redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {selectDirectorySections} from '../../redux/directory/directory.selector'
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
@@ -12,5 +15,7 @@ const Directory = ({sections})=> (
         }
     </div>   
 )
-const mstp = ({})=>{}
+const mstp = createStructuredSelector({
+    sections: selectDirectorySections
+})
 export default connect(mstp)(Directory);
