eb0a84f33774e5682b7d1d4eb9736ecfc0391d7b
corina <corina.schambacher@gmail.com>
2021-01-26 13:59:29 -0800

    collection  page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selector'
 
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => (
-    <div className='collection-page'>
+const CollectionPage = ({collection}) => {
+    console.log(collection)
+   return ( <div className='collection-page'>
         <h2>
             CATEGORY PAGE
         </h2>
-    </div>
-)
+    </div>)
+}
 
-export default CollectionPage;
+const mstp = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mstp)(CollectionPage);
