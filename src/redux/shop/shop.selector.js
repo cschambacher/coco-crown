28b3bcb9c9d95927bffc0054d5f2d82b3f070251
corina <corina.schambacher@gmail.com>
2021-02-01 09:26:18 -0800

    refactored

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -15,6 +15,10 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
 
 export const selectCollection = memoize((collectionUrlParam) =>
   createSelector(
