f77b9b39e08cf94f8a7744ef094a198301280676
corina <corina.schambacher@gmail.com>
2021-02-16 15:59:50 -0800

    transform shop component into class component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,11 +5,16 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component'
 
 
-const ShopPage = ({match}) => (   
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-    </div>   
-)
+class ShopPage extends React.Component {
+    render(){
+        const {match} = this.props
+        return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        </div>   
+        )
+    }
+}
 
 export default ShopPage
