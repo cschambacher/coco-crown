264332c7ca069ecf2de2f02601e6a6809c1df6e8
corina <corina.schambacher@gmail.com>
2021-02-17 15:18:35 -0800

    fixed bug

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -52,14 +52,18 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd)=>{
     return await batch.commit()
 }
 
-const convertCollectionsSnapshotToMap = (collections)=>{
-    const transformedCollection = collection.docs.map(doc => {
+export const convertCollectionsSnapshotToMap = (collections)=>{
+    const transformedCollection = collections.docs.map(doc => {
         const {title, items} = doc.data();
 
         return {
-            routeName: encodeURI(title.toLowerCase())
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
         }
     })
+    console.log(transformedCollection)
 }
 
 export const auth = firebase.auth();
