688dad03040726e4844a55831a9ca9d0a5ca2eb6
corina <corina.schambacher@gmail.com>
2021-02-18 17:17:53 -0800

    modified shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {createStructuredSelector} from 'reselect'
 import {connect} from 'react-redux'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component'
 
-import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
-import {updateCollections} from '../../redux/shop/shop.actions' 
+import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions' 
 import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
@@ -14,18 +14,13 @@ const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
 
-    state = {
-        loading: true
-    }
-
-    unsubscribeFromSnapshot = null
     componentDidMount(){
-        const { updateCollections} = this.props;
+        
        
     }
     render(){
         const {match} = this.props
-        const {loading} = this.state
+       
         return (
         <div className='shop-page'>
             <Route exact path={`${match.path}`} render={(props)=> <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>}/>
