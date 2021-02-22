0a7c25c3bb9abf06c598f08a370ccc8725b0c82f
corina <corina.schambacher@gmail.com>
2021-02-22 12:45:05 -0800

    added new selector in shop component to fix bug

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component'
 
 import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions' 
-import {selectIsCollectionFetching} from '../../redux/shop/shop.selector' 
+import {selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selector' 
 import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
@@ -21,19 +21,20 @@ class ShopPage extends React.Component {
        
     }
     render(){
-        const {match, isCollectionFetching} = this.props
+        const {match, isCollectionFetching, isCollectionsLoaded} = this.props
        
         return (
         <div className='shop-page'>
             <Route exact path={`${match.path}`} render={(props)=> <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props}/>}/>
-            <Route path={`${match.path}/:collectionId`} render={(props)=> <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>}/>
+            <Route path={`${match.path}/:collectionId`} render={(props)=> <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>}/>
         </div>   
         )
     }
 }
 
 const mstp = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
 })
 const mdtp = dispatch => ({
     fetchCollectionsStartAsync: ()=> dispatch(fetchCollectionsStartAsync())
