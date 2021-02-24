6853be3783248183036ef48e22453c0e4ba37616
corina <corina.schambacher@gmail.com>
2021-02-23 16:13:25 -0800

    added saga to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,21 @@ import {persistStore} from 'redux-persist';
 
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// sagaMiddleware.run()
 
 export const persistor = persistStore(store);
 
