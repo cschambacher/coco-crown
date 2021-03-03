5d7daa69711c58a0f305608d6cd3a807ef80bb98
corina <corina.schambacher@gmail.com>
2021-03-03 10:45:28 -0800

    added googleSignInStart to sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { connect } from 'react-redux'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component';
 
 import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions'
 
 import './sign-in.styles.scss';
 
@@ -36,7 +37,7 @@ class SignIn extends React.Component{
     }
 
     render(){
-        
+        const {googleSignInStart} = this.props;
         return(
             
             <div className='sign-in'>
@@ -62,7 +63,7 @@ class SignIn extends React.Component{
                 />
                  <div className='buttons'>
                     <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+                    <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn> 
                     {' '}
                     Sign in with Google{' '} </CustomButton>
                  </div>
@@ -71,6 +72,8 @@ class SignIn extends React.Component{
         )
     }
 }
-
-export default SignIn
+const mdtp = dispatch => ({
+    googleSignInStart: ()=> dispatch(googleSignInStart())
+})
+export default connect(null, mdtp)(SignIn)
 
