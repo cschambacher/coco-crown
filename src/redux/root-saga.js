e918b776136623aa97acac90755e2c69689d31f2
corina <corina.schambacher@gmail.com>
2021-03-03 10:07:31 -0800

    user types

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -3,6 +3,7 @@ import {all, call} from 'redux-saga/effects'
 import { fetchCollectionsStart} from './shop/shop.sagas'
 
 export default function* rootSaga(){
+    // all runs all the code concurrently side by side
     yield all([
         call(fetchCollectionsStart)
     ])
