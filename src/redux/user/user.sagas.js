30d826accf99a1fdbb2c4963e5b7b9a4af68b306
corina <corina.schambacher@gmail.com>
2021-03-05 12:03:18 -0800

    signout saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -58,14 +58,14 @@ export function* isUserAuthenticated() {
   }
 }
 
-// export function* signOut() {
-//   try {
-//     yield auth.signOut();
-//     yield put(signOutSuccess());
-//   } catch (error) {
-//     yield put(signOutFailure(error));
-//   }
-// }
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailure(error));
+  }
+}
 
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
@@ -79,15 +79,15 @@ export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
-// export function* onSignOutStart() {
-//   yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
-// }
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
 
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(onCheckUserSession),
-    // call(onSignOutStart)
+    call(onSignOutStart)
   ]);
 }
