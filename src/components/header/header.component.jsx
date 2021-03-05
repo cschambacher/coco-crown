2343c88dc133705f5b7df8bd7f8d8d037280f258
corina <corina.schambacher@gmail.com>
2021-03-05 11:58:24 -0800

    header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,10 +9,11 @@ import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import {signOutStart} from '../../redux/user/user.actions'
 
 import './header.styles.scss';
 
-const Header = ({currentUser, hidden}) => (
+const Header = ({currentUser, hidden, signOutStart}) => (
     <div className='header'>
          <Link to='/'>
             CoCo <Logo className='logo'/>
@@ -27,7 +28,7 @@ const Header = ({currentUser, hidden}) => (
              
             {
             currentUser ?(
-                <div className='option' onClick={()=> auth.signOut()}>
+                <div className='option' onClick={signOutStart}>
                     SIGN OUT
                 </div>
 
@@ -52,4 +53,7 @@ const mstp = createStructuredSelector({
     currentUser: selectCurrentUser,
     hidden: selectCartHidden
 })
-export default connect(mstp)(Header);
+const mdtp = dispatch => ({
+    signOutStart: ()=> dispatch(signOutStart()),
+})
+export default connect(mstp, mdtp)(Header);
