95594889b2c35646f9b7b2406fc8bdb605d71d2d
corina <corina.schambacher@gmail.com>
2021-03-15 08:13:08 -0700

    useEffect in shopPage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
 
 import {connect} from 'react-redux'
@@ -12,23 +12,18 @@ import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
 
 
-class ShopPage extends React.Component {
+const ShopPage = ({match, fetchCollectionsStart}) => {
+    useEffect(()=>{
+        fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
+    
+    return (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
+    </div>   
+    )
 
-    componentDidMount(){
-        const {fetchCollectionsStart} = this.props;
-        fetchCollectionsStart()
-       
-    }
-    render(){
-        const {match} = this.props
-       
-        return (
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}/>
-            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
-        </div>   
-        )
-    }
 }
 
 
