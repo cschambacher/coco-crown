b12eeb1749edd0532cab357f214a20a34ffb0943
corina <corina.schambacher@gmail.com>
2021-03-16 20:48:20 -0700

    added backend and payment route for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51IG6YGCcG5ePhRT5MXukl7do0PTUoHDwAomqqMXCqN7bpFRtxl1zrUo4BCKbKMNaNDw2kTHTLyeApicYfA6stqnH00afDUyW5z';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(response => {
+            alert('Payment Successful')  
+        }).catch(error => {
+            console.log('Payment error: ', error);
+            alert('There was an issue with your payment. Please make sure you use the provided credit card. ')
+        });
     }
     return (
         <StripeCheckout
