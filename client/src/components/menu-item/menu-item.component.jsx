22c6081ee0a0a71867cec332ec51b0bab5cb5f23
corina <corina.schambacher@gmail.com>
2021-04-06 10:37:00 -0700

    added styled components and responsivness to menu-item

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -17,6 +17,33 @@ const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
+);import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle
+} from './menu-item.styles';
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <MenuItemContainer
+    size={size}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <BackgroundImageContainer
+      className='background-image'
+      imageUrl={imageUrl}
+    />
+    <ContentContainer className='content'>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>
 );
+
+export default withRouter(MenuItem);
 
 export default withRouter(MenuItem);
